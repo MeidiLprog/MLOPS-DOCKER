96d22fe23275116c9bb01113db302bb9bbe5b631
MeidiLprog <m.lef3105@gmail.com>
2026-02-19 17:51:26 +0100

    trainmodel() function

--- image_ml_service_tp/main.py
+++ image_ml_service_tp/main.py
@@ -1,25 +1,75 @@
 import os
-import random
-from flask import Flask
+import random as r
+from flask import Flask, jsonify
+import requests as req
+from datetime import datetime
+import numpy as np
+#SKLEARN imports
+
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split, StratifiedKFold, cross_val_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score,recall_score,precision_score
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def entry():
-    nb = [(random.randint(1,100)%15) for _ in range(10)]
+"""
+Simple random number generator, one list, and assigning a random value to a <td> element in html
+"""
 
-    html = "<html><body><table>"
-    for i in nb:
+#root directory
+@app.route('/')
+def randitup() -> str:
+   nb  = [(r.randint(1,1000) % 10) for _ in range(10)]
+   
+   html = "<html><body><table>"
+   for i in nb:
       html += f"<tr><td>{i}</td></tr>"
-    html += "</table></body></html>"
-    return html
+   html += "</body></html>"
+   html += '<br><a href="/train">Train model</a>'
+   html += '<br><a href="/results">Results</a>'
+   html += '<br><a href="/call-ml">Test an ML service</a>'
+
+   html += "</body></html>"
+   return html
+
+#global variables needed for the resultats
+accuracy : float = None
+precision : float = None
+recall : float = None
+
+trained = None
+
+@app.route('/train')
+def trainModel():
+   global accuracy,precision,recall
+
+   X,y = make_classification(n_samples=2000,n_features=20,n_informative=10,flip_y=0.02,random_state=42)
+
+   X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42,stratify=y)
+   cv = StratifiedKFold(n_splits=5,shuffle=True,random_state=42)
+   model = RandomForestClassifier(n_estimators=100,random_state=42,n_jobs=-1)
+   scores = cross_val_score(model,X_train,y_train,cv=cv,scoring="roc_auc")
+   print(f"AUC score mean: {scores.mean()} AUC score var: {scores.std()}\n")
+   print(f"Test score: {model.score(X_test,y_test)}")
+
+@app.route('/results')
+def results():
+   if accuracy is None:
+      return "No model has been trained \n"
+   return f"""
+   <html><body>
+      <h1>Results</h1>
+      <p>Accuracy: {accuracy:.3f}</p>
+      <p>Precision: {precision:.3f}</p>
+      <p>Recall: {recall:.3f}</p>
+      <p>Timestamp: {trained:.3f}</p>
+   </body></html>
+   """
+
+
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0',debug=True,port=5000)
-
-
-
-
-
-
-
