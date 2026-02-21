d109e8148d0e8d065e5e0bfcd2d98c5df2e6c0bb
MeidiLprog <m.lef3105@gmail.com>
2026-02-21 23:16:16 +0100

    config file ready

---
+++ image2_ml_service/config.py
@@ -0,0 +1,20 @@
+#configuration
+#ML SERVICE set up
+
+
+DEBUG = False
+PORT = 5001
+IP = "0.0.0.0"
+
+DEFAULT_LANGUAGE = "en"
+
+
+#Models spacy shall us
+SPACY_MODELS = {
+    "en" : "en_core_web_ms",
+    "fr" : "fr_core_web_ms",
+}
+
+#Directories
+DAT_DIR = "data"
+MODELS_DIR = "models"
