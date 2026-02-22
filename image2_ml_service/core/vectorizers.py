6f5510b73c12b60eb25bda659edb4043404df9f3
MeidiLprog <m.lef3105@gmail.com>
2026-02-22 18:19:08 +0100

    start to work on class Vectorize

---
+++ image2_ml_service/core/vectorizers.py
@@ -0,0 +1,31 @@
+from sklearn.feature_extraction import TfidfVectorizer, CountVectorizer
+import numpy as np
+import os
+from typing import Tuple,List,Optional
+
+
+class Vectorize:
+    def __init__(self,method : str = "tfidf", max_features=500):
+
+        self.model = None
+        self.max_features = max_features
+        self.method = method
+        self.features_names = None #quite useful to retrieve the words
+
+
+    def fit_transform(self, text : List[str]) -> np.ndarray:
+        
+        if self.method == "tfidf":
+            self.model = TfidfVectorizer(max_features=self.max_features)
+        elif self.method == "count":
+            self.model = CountVectorizer(max_features=self.max_features)
+        else:
+            raise ValueError("You may only pick a model amongst tfidf or countvectorizer \n")
+        X = self.model.fit_transform(text)
+        self.features_names = self.model.get_feature_names_out()
+        return X.toarray()
+    
+    
+    
+
+
